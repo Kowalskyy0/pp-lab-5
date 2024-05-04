82b907299b82184c69c1adf2fa618f3de5b59edc
Mariusz <koval09603@gmail.com>
2024-05-04 18:24:44 +0200

    zad

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import company.beans.*;
+import company.exceptions.*;
 import company.implementations.*;
 import company.interfaces.*;
 import company.utils.*;
@@ -5,13 +7,23 @@ import company.utils.*;
 public class Main {
 
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-
-        int sum = MathUtils.add(a, b);
-        System.out.println("Suma: " + sum);
-
+        Person[] people = new Person[5];
+        final int b = 10;
         Messenger messenger = new EmailMessenger();
-        messenger.sendMessage("Suma " + a + " i " + b + " wynosi: " + sum);
+        try {
+            people[0] = new Person("Jan Kowalski", 30);
+            people[1] = new Person("Anna Nowak", 25);
+            people[2] = new Person("Piotr Zieliński", 40);
+            people[3] = new Person("Marta Wiśniewska", 35);
+            people[4] = new Person("Krzysztof Górski", 50);
+
+            for (Person person : people) {
+                int result = MathUtils.add(person.getAge(), b);
+                messenger.sendMessage("Wynik dodania wieku " + person.getName() + " (" + person.getAge() + ") i " + b
+                        + " wynosi: " + result);
+            }
+        } catch (InvalidAgeException e) {
+            System.err.println("Błąd: " + e.getMessage());
+        }
     }
 }
